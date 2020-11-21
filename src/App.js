d9dd1ab983f84195bcda9169f90f79cafe54993d
rin1208 <sora1208sky@gmail.com>
2020-11-22 03:51:16 +0900

    set up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import Content from './pages/Content'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import SignIn from './pages/SignIn'
-import styled from "@emotion/styled"
 
 
 function App() {
